0f6d9b0664af3d007a3c65633ae47af8e70b96dd
manash <manashmndl@gmail.com>
2017-07-26 01:33:37 +0600

    add js file

--- app/static/js/network.js
+++ app/static/js/network.js
@@ -1,5 +1,42 @@
+// create an array with nodes
+  var nodes = new vis.DataSet([
+    {id: 1, label: 'Node 1'},
+    {id: 2, label: 'Node 2'},
+    {id: 3, label: 'Node 3'},
+    {id: 4, label: 'Node 4'},
+    {id: 5, label: 'Node 5'}
+  ]);
 
-$("#keywordSearchInput").on('keypress', function(d){
-    console.log("HELLO");
+  // create an array with edges
+  var edges = new vis.DataSet([
+    {from: 1, to: 3},
+    {from: 1, to: 2},
+    {from: 2, to: 4},
+    {from: 2, to: 5},
+    {from: 3, to: 3}
+  ]);
+
+  // create a network
+  var container = document.getElementById('mynetwork');
+  var data = {
+    nodes: nodes,
+    edges: edges
+  };
+  var options = {};
+  var network = new vis.Network(container, data, options);
+
+
+
+
+  // ------------- Component Size Calculations ------------------- //
+
+$(document).ready(function(){
+    $(window).resize(function(){
+        // update current window height and width
+        let window_height = $(window).height();
+        let window_width = $(window).width();
+
+        console.log("window height: " + window_height);
+  });
 });
-
+  
