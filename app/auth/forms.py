0d5ef15586f7a70fce704eb6c93c7d0eb62574e5
manash <manashmndl@gmail.com>
2017-07-25 19:26:11 +0600

    fix css file

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,11 +1,11 @@
-from wtforms import Form, TextField, validators
+from wtforms import Form, StringField, validators
 from wtforms.validators import DataRequired
 from flask import render_template
 
 from . import auth
 
 class LoginForm(Form):
-    username = TextField('lg_username', validators=[DataRequired()])
+    username = StringField('lg_username', validators=[DataRequired()])
 
 
 
