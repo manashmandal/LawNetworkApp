12b352462745b07c8ed4e30ccb58b248536fabee
manash <manashmndl@gmail.com>
2017-07-26 01:45:03 +0600

    update network

--- app/static/js/network.js
+++ app/static/js/network.js
@@ -1,30 +1,35 @@
-// create an array with nodes
-  var nodes = new vis.DataSet([
-    {id: 1, label: 'Node 1'},
-    {id: 2, label: 'Node 2'},
-    {id: 3, label: 'Node 3'},
-    {id: 4, label: 'Node 4'},
-    {id: 5, label: 'Node 5'}
-  ]);
 
-  // create an array with edges
-  var edges = new vis.DataSet([
-    {from: 1, to: 3},
-    {from: 1, to: 2},
-    {from: 2, to: 4},
-    {from: 2, to: 5},
-    {from: 3, to: 3}
-  ]);
+  function drawNetwork(){
+    // create an array with nodes
+    var nodes = new vis.DataSet([
+        {id: 1, label: 'Node 1'},
+        {id: 2, label: 'Node 2'},
+        {id: 3, label: 'Node 3'},
+        {id: 4, label: 'Node 4'},
+        {id: 5, label: 'Node 5'}
+    ]);
 
-  // create a network
-  var container = document.getElementById('mynetwork');
-  var data = {
-    nodes: nodes,
-    edges: edges
-  };
-  var options = {};
-  var network = new vis.Network(container, data, options);
+    // create an array with edges
+    var edges = new vis.DataSet([
+        {from: 1, to: 3},
+        {from: 1, to: 2},
+        {from: 2, to: 4},
+        {from: 2, to: 5},
+        {from: 3, to: 3}
+    ]);
 
+    // create a network
+    var container = document.getElementById('mynetwork');
+    var data = {
+        nodes: nodes,
+        edges: edges
+    };
+    var options = {};
+    var network = new vis.Network(container, data, options);
+  }
+
+
+  drawNetwork();
 
 
 
@@ -44,6 +49,9 @@ $(document).ready(function(){
         console.log("Footer height: " + footer_height);
 
         $("#mynetwork").css('height', window_height - footer_height - navbar_height);
+        
+        // Redraws the network 
+        setTimeout(drawNetwork(), 1000);
   });
 });
   
