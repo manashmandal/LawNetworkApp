5971ab322214d7d5599d3ffe3b6f027b40027073
manash <manashmndl@gmail.com>
2017-07-25 23:56:42 +0600

    remove unnecessary files

--- app/viz/views.py
+++ app/viz/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from . import viz
+from flask_login import login_required
 
 @viz.route('/viz')
+# @login_required
 def visualization():
-    return render_template('viz_base.html')
+    return render_template('viz.html')
