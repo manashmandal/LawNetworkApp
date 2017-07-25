635c032f3a54aca9a8768c14c9e253cb09a300ec
manash <manashmndl@gmail.com>
2017-07-25 17:49:15 +0600

    add forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,14 +1,3 @@
-from . import auth
-from flask import render_template
-from config import config
-from app import mongo
-
-
-# @auth.route('/login')
-# def login_page():
-#     print(mongo.db.users.find_one({'id' : 1}))
-#     return render_template('login.html', title='Login')
-
 from wtforms import Form, TextField, validators
 from wtforms.validators import DataRequired
 from flask import render_template
@@ -25,6 +14,4 @@ def login_page():
     form = LoginForm()
     return render_template('login.html', form=form)
 
-@auth.route('/register')
-def register_page():
-    return render_template('register.html', title='Register')
+
