3885af4d5e061e21afcb5fdfadf6f0286b7a7ee4
manash <manashmndl@gmail.com>
2017-07-25 22:17:23 +0600

    add FlaskForm

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,17 +1,17 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 from flask import render_template
 
 from . import auth
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     lg_username = StringField(_name='lg_username', validators=[DataRequired()])
     lg_password = PasswordField(_name='lg_password', validators=[DataRequired()])
     # submit = SubmitField("")
 
 
-class RegistrationForm(Form):
+class RegistrationForm(FlaskForm):
     reg_username = StringField(_name='reg_username', validators=[DataRequired()])
     reg_password = PasswordField(_name='reg_password', validators=[DataRequired()])
     reg_password_confirm = PasswordField(_name='reg_password', validators=[DataRequired()])
