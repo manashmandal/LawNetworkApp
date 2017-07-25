cc5d5c8d8dbf8a4ea20a5e5d25bc76dc7a4a62fb
manash <manashmndl@gmail.com>
2017-07-25 12:59:17 +0600

    add flask file

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('base.html', title="Homepage")
+
+@app.route('/extend')
+def extend():
+    return render_template('extend.html', title='EXTEND BIATCH')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
