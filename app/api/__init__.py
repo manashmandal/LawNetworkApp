31d3584639b5e252593b6cbc78e9094c55c85b97
manash <manashmndl@gmail.com>
2017-07-26 11:23:16 +0600

    fix bug

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,12 +1,5 @@
 from flask import Blueprint
-from app import mongo
-
-INNER_LAW_NETWORK = mongo.db.network # This network collection contains the relationship between named entities and sections
-LAW_NETWORK = "" # Outer law network, which cites which one
-LAWS = mongo.db.laws # Containes the law texts
 
 api = Blueprint('api', __name__)
-
-
 
 from . import routes
--- app/api/routes.py
+++ app/api/routes.py
@@ -1,14 +1,20 @@
 from app import mongo
-from . import (api, LAW_NETWORK, INNER_LAW_NETWORK, LAWS)
+from . import api
 from flask_login import login_required
 from flask import request
+
+# INNER_LAW_NETWORK = mongo.db.network # This network collection contains the relationship between named entities and sections
+# LAW_NETWORK = "" # Outer law network, which cites which one
+# LAWS = mongo.db.laws # Containes the law texts
+# USERS = mongo.db.users
+
 
 # Returns the connection [Main Connection]
 @api.route('/api/connection', methods=['GET'])
 # @login_required
 def get_law_connection():
     law_id = request.args.get('id')
-    return "Connnection %s" % str(law_id)
+    return "Connnection %s" % str(mongo.db.users.find_one({'username' : 'manash'}))
 
 
 # Amendment data
