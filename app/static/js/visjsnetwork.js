5fd37f714bce0c6a520f7fc2dab512b686ff35d2
manash <manashmndl@gmail.com>
2017-07-28 03:28:40 +0600

    adding opacity change

--- app/static/js/visjsnetwork.js
+++ app/static/js/visjsnetwork.js
@@ -1,10 +1,16 @@
+
+var connected_edges;
+var all_edges;
+var all_edge_ids = [];
+
+//TODO: Optimize
 function drawNetwork (data){
 
     let nodes = [];
     let edges = [];
 
     for (var i = 0; i < data.laws.length; i++){
-        nodes.push({id : data.laws[i]});
+        nodes.push({id : data.laws[i], label: "" + data.laws[i], title: data.id_title_map[data.laws[i]]});
     }
 
     for (var i = 0; i < data.network.length; i++){
@@ -14,22 +20,81 @@ function drawNetwork (data){
     // Create data
 
 
-    console.log(nodes);
-    console.log(edges);
+    let _nodes = new vis.DataSet(nodes);
+    let _edges = new vis.DataSet(edges);
+
+    all_edges = _edges;
+
+    // Getting all data
+    for (key in all_edges._data){
+        all_edge_ids.push(key);
+    }
 
     var container = document.getElementById("mynetwork");
 
     var _data = {
-        nodes: nodes,
-        edges: edges
+        nodes: _nodes,
+        edges: _edges
     };
+
 
     var options = {
-        nodes: {
-            shape: 'dot',
-            size: 10
+        "nodes": {
+            "borderWidthSelected": 8
+        },
+        "edges": {
+            "arrows": {
+            "to": {
+                "enabled": true,
+                "scaleFactor" : 0.7
+            },
+            "from": {
+                "enabled": true,
+                "scaleFactor" : 0.7
+            }
+            },
+            "scaling": {
+            "min": 39,
+            "max": 73
+            },
+            "smooth": {
+            "forceDirection": "none"
+            }
+        },
+        "interaction": {
+            "hover": true
+        },
+        "physics": {
+            "minVelocity": 0.75
         }
-    };
+        };
+
+    // var options = {
+    //     nodes: {
+    //         shape: 'dot',
+    //         size: 15
+    //     }
+    // };
 
     var network = new vis.Network(container, _data, options);
+
+    console.log("ALL EDGE ID");
+    console.log(all_edge_ids);
+
+    // On Double click reset it 
+    network.on('selectNode', function(params){
+
+        let remaining_ids = _.difference(
+            all_edge_ids, params.edges
+        )
+
+        console.log("REMAINING EDGES");
+        console.log(remaining_ids);        
+        
+        // Add less opacity
+        for (var i = 0; i < remaining_ids.length; i++){
+            let connected_edge = _edges.get(remaining_ids[i]);
+            console.log("CONNECTED EDGE: " + connected_edge);
+        }
+    });
 }
