6fbcf3eed39f1d8032afdd1ae5509c4130c44c55
manash <manashmndl@gmail.com>
2017-07-28 01:58:09 +0600

    update

--- app/static/js/network.js
+++ app/static/js/network.js
@@ -26,39 +26,6 @@ function updateComponentMeasurements(){
 }
 
 
-// function drawNetwork(){
-//     // create an array with nodes
-//     var nodes = new vis.DataSet([
-//         {id: 1, label: 'Node 1'},
-//         {id: 2, label: 'Node 2'},
-//         {id: 3, label: 'Node 3'},
-//         {id: 4, label: 'Node 4'},
-//         {id: 5, label: 'Node 5'}
-//     ]);
-
-//     // create an array with edges
-//     var edges = new vis.DataSet([
-//         {from: 1, to: 3},
-//         {from: 1, to: 2},
-//         {from: 2, to: 4},
-//         {from: 2, to: 5},
-//         {from: 3, to: 3}
-//     ]);
-
-//     // create a network
-//     var container = document.getElementById('mynetwork');
-//     var data = {
-//         nodes: nodes,
-//         edges: edges
-//     };
-//     var options = {};
-//     var network = new vis.Network(container, data, options);
-//   }
-
-
-
-
-
 
 
   // Component Size Calculation
@@ -111,6 +78,7 @@ var loadLawTitles = function(data){
 
     drawNetwork(data);
 
+    loadingDone();
 }
 
 $(document).ready(function(){
@@ -147,9 +115,7 @@ $(document).ready(function(){
             exclude_unigram: _exclude_unigram
         }, function(data){
             loadLawTitles(data);
-        })
-        .then(loadingDone)
-        .then(updateComponentMeasurements);
+        });
 
         console.log(search_keywords);
     });
--- app/static/js/visjsnetwork.js
+++ app/static/js/visjsnetwork.js
@@ -11,6 +11,25 @@ function drawNetwork (data){
         edges.push({from : data.network[i].from, to : data.network[i].to });
     }
 
+    // Create data
+
+
     console.log(nodes);
     console.log(edges);
+
+    var container = document.getElementById("mynetwork");
+
+    var _data = {
+        nodes: nodes,
+        edges: edges
+    };
+
+    var options = {
+        nodes: {
+            shape: 'dot',
+            size: 10
+        }
+    };
+
+    var network = new vis.Network(container, _data, options);
 }
