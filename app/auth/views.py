2f8cc0fc5cf8584543f2d5023c25908e8ece8fa7
Manash <manashmndl@gmail.com>
2017-08-02 17:47:02 +0600

    remove unnecessary codes and add flash message on login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,9 @@
 from . import auth
 from .forms import RegistrationForm, LoginForm
-from flask import render_template, redirect, request, url_for
+from flask import (render_template, redirect, request, url_for, flash)
 from app import mongo
 from app.models import User, load_user
-from flask_login import login_user, login_required, logout_user
+from flask_login import (login_user, login_required, logout_user)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -12,11 +12,16 @@ def login_page():
     form = LoginForm()
     if form.validate_on_submit():
         user = mongo.db.users.find_one({'username' : form.lg_username.data })
-        the_user = load_user(user['username'])
-        print(user)
-        if user is not None and User.validate_login(user['password'], form.lg_password.data):
-            login_user(the_user)
-            return redirect(request.args.get('next') or url_for('main.index'))
+        try:
+            the_user = load_user(user['username'])
+            print(user)
+        except:
+            # if user is not None and User.validate_login(user['password'], form.lg_password.data):
+            #     login_user(the_user)
+            #     return redirect(request.args.get('next') or url_for('main.index'))
+            # else:
+            flash("Username Or Password didn't match")
+        return redirect(url_for('auth.login_page'))
     return render_template('login.html', form=form)
 
 
@@ -31,15 +36,29 @@ def logout():
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register_page():
+    error = None
     form = RegistrationForm()
     if form.validate_on_submit():
         reg_username = form.reg_username.data
         reg_password = form.reg_password.data
         reg_password_confirm = form.reg_password_confirm.data
         reg_email = form.reg_email.data
-
         print("USERNAME : " + reg_username)
+
+        # Check the password if matched
+        ## TODO: If not matched show a flash message
         if (reg_password == reg_password_confirm):
             print("PAssword matched")
             return redirect(request.args.get('next') or url_for('main.index'))
+
+        # If password matches then check for username if it exists or not
+        if (mongo.db.users.find_one({'username' : reg_username}) != None):
+            # TODO: Show flash message that users exists
+            print("USER EXISTS")
+            return redirect(url_for('auth.register_page'))
+        else:
+            print("Checking for username")
+            print(mongo.db.users.find_one({'username' : reg_username}))
+
+
     return render_template('register.html', form=form)
