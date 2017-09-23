34b1ca9dd17b61e1d77eaf714ba92d52fbbcfdfa
Manash <manashmndl@gmail.com>
2017-09-23 18:48:36 +0600

    add tagcloud

--- app/static/js/tagcloud.js
+++ app/static/js/tagcloud.js
@@ -1,6 +1,48 @@
 var DAT;
 
 
+function draw(words_map){
+
+    $("#tagcloud").empty();
+
+    var fill = d3.scale.category20();
+    // var words = ;
+
+    d3.layout.cloud().size([300, 300])
+                     .words(words_map.map(function(d){
+                        return {text: d.word, size: d.freq};
+                      }))
+                     .padding(5)
+                     .rotate(function() { return ~~(Math.random() * 2) * 90; })
+                     .font("Impact")
+                     .fontSize(function(d) { return d.size; })
+                     .on('end', draw)
+                     .start();
+
+    function draw(words){
+        d3.select('#tagcloud').append('svg')
+                .attr('width', 300)
+                .attr('height', 300)
+            .append('g')
+                .attr('transform', "translate(150, 150)")
+            .selectAll('text')
+                .data(words)
+            .enter().append('text')
+                .style('font-size', function(d) { return d.size + 'px'; })
+                .style('font-family', "Impact")
+                .style('fill', function(d, i) { return fill(i); })
+                .attr('text-anchor', 'middle')
+                .attr('transform', function(d){
+                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                })
+                .text(function(d) { return d.text; });
+
+    }
+    
+    
+}
+
+
 function drawTagCloud(_data, law_id, stopLoading){
 
         // Calculate available area
@@ -50,9 +92,10 @@ function drawTagCloud(_data, law_id, stopLoading){
                 id: law_id,
                 key: section_key
             }).done(function(response){
+                draw(response.info.words);
                 console.log(response);
             });
-            
+
         });
 
         $("#lawModal").modal('toggle');
