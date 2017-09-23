df01b7bf23a9e1a8e8a6026a8e8c0ce3e6660013
Manash <manashmndl@gmail.com>
2017-09-23 20:10:19 +0600

    add highlight

--- app/static/js/tagcloud.js
+++ app/static/js/tagcloud.js
@@ -1,7 +1,7 @@
 var DAT;
-var threshold_frequency = 2;
+var temp;
 
-function draw(words_map){
+function draw(words_map) {
 
     $("#tagcloud").empty();
     var current_hover_tag = "";
@@ -10,105 +10,106 @@ function draw(words_map){
     // var words = ;
 
     d3.layout.cloud().size([500, 500])
-                     .words(words_map.map(function(d){
-                        return {text: d.word, size: d.freq};
-                      }))
-                     .padding(5)
-                     .rotate(function() { return ~~(Math.random() * 2) * 90; })
-                     .font("Impact")
-                     .fontSize(function(d) { return d.size; })
-                     .on('end', draw)
-                     .start();
+        .words(words_map.map(function (d) {
+            return { text: d.word, size: d.freq };
+        }))
+        .padding(5)
+        .rotate(function () { return ~~(Math.random() * 2) * 90; })
+        .font("Impact")
+        .fontSize(function (d) { return d.size; })
+        .on('end', draw)
+        .start();
 
-    function draw(words){
+    function draw(words) {
         d3.select('#tagcloud').append('svg')
-                .attr('width', 500)
-                .attr('height', 500)
+            .attr('width', 500)
+            .attr('height', 500)
             .append('g')
-                .attr('transform', "translate(250, 250)")
+            .attr('transform', "translate(250, 250)")
             .selectAll('text')
-                .data(words)
+            .data(words)
             .enter().append('text')
-                .style('font-size', function(d) { return 5+d.size + 'px'; })
-                .style('font-family', "Impact")
-                .style('fill', function(d, i) { return fill(i); })
-                .attr('text-anchor', 'middle')
-                .attr('transform', function(d){
-                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-                })
-                .text(function(d) { return d.text; });
+            .style('font-size', function (d) { return 5 + d.size + 'px'; })
+            .style('font-family', "Impact")
+            .style('fill', function (d, i) { return fill(i); })
+            .attr('text-anchor', 'middle')
+            .attr('transform', function (d) {
+                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .text(function (d) { return d.text; });
 
     }
 
 
     // Shows tooltip
-    $("text").each(function() { 
-        $(this).qtip({ 
+    $("text").each(function () {
+        $(this).qtip({
             content: {
-                text: $(this).text() 
+                text: $(this).text()
             }
         });
     });
-    
+
 }
 
 
-function drawTagCloud(_data, law_id, stopLoading){
+function drawTagCloud(_data, law_id, stopLoading) {
 
-        // Calculate available area
-        let clearance = 50;
+    // Calculate available area
+    let clearance = 50;
 
-        // Resize viz
-        $("#viz").css('height', $(window).height() - clearance + 'px');
-        $("#viz").css('width', $(window).width() / 2.2 - clearance + 'px');
-        
-        // Resize modal
-        $(".modal").css('min-height', $(window).height() + 'px');
-        $(".modal").css('max-height', $(window).height()   + 'px');
-        $(".modal-body").css('height', $(window).height() - clearance + 'px');
-        $(".modal-body").css('max-height', $(window).height() - clearance  + 'px');
+    // Resize viz
+    $("#viz").css('height', $(window).height() - clearance + 'px');
+    $("#viz").css('width', $(window).width() / 2.2 - clearance + 'px');
 
-        
-        // stopLoading();
+    // Resize modal
+    $(".modal").css('min-height', $(window).height() + 'px');
+    $(".modal").css('max-height', $(window).height() + 'px');
+    $(".modal-body").css('height', $(window).height() - clearance + 'px');
+    $(".modal-body").css('max-height', $(window).height() - clearance + 'px');
 
 
-        // Append the sections
-        $("#sections").append("<ul>");
-        for (let i = 0; i < _data['section_keys'].length; i++){
-            // $("#sections").append("<span class='label label-default>'" + _data['section_keys'][i] + "</label>");
-            // $("#sections").append(data['section_keys'][i]);
+    // stopLoading();
 
-            let value = _data['section_keys'][i];
-            if (value !== ""){
-                $("#sections").append("<li class='section_keys' id='section_" + i + "'>" + value + "</li>");
-            }
-                // </br></br>
+
+    // Append the sections
+    $("#sections").append("<ul>");
+    for (let i = 0; i < _data['section_keys'].length; i++) {
+        // $("#sections").append("<span class='label label-default>'" + _data['section_keys'][i] + "</label>");
+        // $("#sections").append(data['section_keys'][i]);
+
+        let value = _data['section_keys'][i];
+        if (value !== "") {
+            $("#sections").append("<li class='section_keys' id='section_" + i + "'>" + value + "</li>");
         }
-        $("#sections").append("</ul>");
+        // </br></br>
+    }
+    $("#sections").append("</ul>");
 
-        $("#sections").css('max-height', $('.modal-body').height() + 'px');
+    $("#sections").css('max-height', $('.modal-body').height() + 'px');
 
-        DAT = _data;
- 
+    DAT = _data;
 
-        // Add event listener to list items 
-        $(".section_keys").on('click', function(){
-            console.log(this.id);
-            
-            let section_key = $("#" + this.id).text();
 
-            // Send get request for data
-            $.getJSON("/api/wordcloud", {
-                id: law_id,
-                key: section_key
-            }).done(function(response){
-                draw(response.info.words);
-                console.log(response);
-            });
+    // Add event listener to list items 
+    $(".section_keys").on('click', function () {
+        // Remove all highlights 
+        $(".section_keys").removeClass('highlight');
+        $(this).addClass('highlight');
+        let section_key = $("#" + this.id).text();
 
+        // Send get request for data
+        $.getJSON("/api/wordcloud", {
+            id: law_id,
+            key: section_key
+        }).done(function (response) {
+            draw(response.info.words);
+            console.log(response);
         });
 
-        $("#lawModal").modal('toggle');
+    });
+
+    $("#lawModal").modal('toggle');
     // console.log(_data);
     // console.log("SELECTED LAW ID " + law_id);
     // stopLoading();
